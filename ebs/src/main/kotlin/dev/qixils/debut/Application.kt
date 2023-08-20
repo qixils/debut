3682b011a6e058044f70c3a00a919737c5fa11e5
Lexi Larkin <lexi@qixils.dev>
2023-08-20 14:22:34 -0400

    fix: unique server port

--- ebs/src/main/kotlin/dev/qixils/debut/Application.kt
+++ ebs/src/main/kotlin/dev/qixils/debut/Application.kt
@@ -10,7 +10,7 @@ import io.ktor.server.netty.*
 import java.util.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module).start(wait = true)
+    embeddedServer(Netty, port = 33288, host = "0.0.0.0", module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
